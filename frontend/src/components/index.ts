ed062fb4dbd021589257265b0e270edc99254ca1
ilynx69 <oleg19763110@gmail.com>
2026-03-11 00:38:36 +0300

    Temp delete

--- frontend/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as ProductCard } from './ProductCard.js';
-export { default as SearchBar } from './SearchBar.js';
-export { default as Filters } from './Filters.js';
-export { default as CartItem } from './CartItem.js';
